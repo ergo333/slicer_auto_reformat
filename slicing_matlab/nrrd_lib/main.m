7b1b06040b0a46512400093070ab4eb07a3f432d
Eros <ghignonieros@gmail.com>
2018-04-10 14:15:46 +0200

    Add files via upload

--- slicing_matlab/nrrd_lib/main.m
+++ slicing_matlab/nrrd_lib/main.m
@@ -7,8 +7,8 @@ N = 1;
 mrPath = "/home/eros/Desktop/Annotated_MRI/Case9-MR.nrrd";
 segPath = "/home/eros/Desktop/Annotated_MRI/Case9-MR-label.nrrd";
 
-outDataPath = "/home/eros/Desktop/Prova/Data/";
-outSegPath = "/home/eros/Desktop/Prova/Label/";
+outDataPath = '/home/eros/Desktop/Prova/Data';
+outSegPath = '/home/eros/Desktop/Prova/Label';
 
 % Leggo il file nrrd che contiene la MR
 [mr, metaMR] = nrrdread(mrPath);
@@ -21,31 +21,56 @@ outSegPath = "/home/eros/Desktop/Prova/Label/";
 mr = double(mr);
 segmentazione = double(segmentazione);
 
-[a b c] = ndgrid(linspace(1, size(mr, 1), 512), ...
-                 linspace(1, size(mr, 2), 512), ...
-                 linspace(1, size(mr, 3), 512));
+[a b c] = ndgrid(linspace(1, size(mr, 1), 256), ...
+                 linspace(1, size(mr, 2), 256), ...
+                 linspace(1, size(mr, 3), 256));
 mrOut = interp3(mr, a, b, c, 'linear');
 mr = double(mrOut);
 
 clearvars a b c;
 
-[a b c] = ndgrid(linspace(1, size(segmentazione, 1), 512), ...
-                 linspace(1, size(segmentazione, 2), 512), ...
-                 linspace(1, size(segmentazione, 3), 512));
+[a b c] = ndgrid(linspace(1, size(segmentazione, 1), 256), ...
+                 linspace(1, size(segmentazione, 2), 256), ...
+                 linspace(1, size(segmentazione, 3), 256));
 segOut = interp3(segmentazione, a, b, c, 'linear');
 segmentazione = double(segOut);
 
 clearvars mrOut segOut;
 
 %% Genero gli slices e salvo le relative immagini 
-for i = 1:N
+for i = 1:2000
     
-    [img, imgSeg] = reformat(mr, segmentazione);
+    [img, imgSeg, xs, ys, zs] = reformat(mr, segmentazione);
+    img = uint16(img);
+    imgSeg = uint8(imgSeg);
+    fileNameDCM = sprintf('MR-%05d.dcm', i);
+    fullNameDCM = fullfile(outDataPath, fileNameDCM);
+    fileName = sprintf('MR-%05d.png', i);
+    fullName = fullfile(outDataPath, fileName);
+    img = mat2gray(img, [0, 2 ^ 16]);
     
-    fileName = sprintf("%sMR-%05d.png", outDataPath, i);
-    imwrite(img, fileName, 'png');
-    fileName = sprintf("%sMR-label-%05d.png", outSegPath, i);
-    imwrite(imgSeg, filename, 'png');
+    imwrite(img, fullName, 'png', 'BitDepth', 16);
+    
+    vertex1 = [xs(1, 1) ys(1, 1) zs(1, 1)];
+    vertex2 = [xs(1, 256) ys(1, 256) zs(1, 256)];
+    vertex3 = [xs(256, 1) ys(256, 1) zs(256, 1)];
+    vertex4 = [xs(256, 256) ys(256, 256) zs(256, 256)];
+    
+    coords = sprintf('(%f, %f, %f), (%f, %f, %f), (%f, %f, %f), (%f, %f, %f)',...
+        vertex1(1), vertex1(2), vertex1(3),...
+        vertex2(1), vertex2(2), vertex2(3),...
+        vertex3(1), vertex3(2), vertex3(3),...
+        vertex4(1), vertex4(2), vertex4(3));
+    
+    dicomwrite(img, fullNameDCM, 'PatientID', coords);
+    
+    fileNameDCM = sprintf('MR-label-%05d.dcm', i);
+    fullNameDCM = fullfile(outSegPath, fileNameDCM);
+    fileName = sprintf('MR-label-%05d.png', i);
+    fullName = fullfile(outSegPath, fileName);
+    
+    imwrite(mat2gray(imgSeg), fullName);
+    dicomwrite(mat2gray(imgSeg), fullNameDCM, 'PatientID', coords);
     
 end
 
--- slicing_matlab/nrrd_lib/reformat.m
+++ slicing_matlab/nrrd_lib/reformat.m
@@ -1,8 +1,8 @@
-function [img, imgSeg] = reformat(mr, segmentazione)
+function [img, imgSeg, xs, ys, zs] = reformat(mr, segmentazione)
 
-    [x, y, z] = meshgrid(1:1:512, 1:1:512, 1:1:512);
+    [x, y, z] = meshgrid(1:1:256, 1:1:256, 1:1:256);
 
-    volSlice = surf(1:1:512, 1:1:512, zeros(length(z)));
+    volSlice = surf(1:1:256, 1:1:256, zeros(length(z)));
     
     % Genero i gradi di rotazione casuali sugli assi
     a = 0;
@@ -10,8 +10,13 @@ function [img, imgSeg] = reformat(mr, segmentazione)
     rotationX = a + (b-a).*rand(1,1);
     rotationY = a + (b-a).*rand(1,1);
     rotationZ = a + (b-a).*rand(1,1);
-    % Genero i valori delle traslazioni
-    % TODO 
+    
+    % Genero lo spostamento sull'asse delle X e delle Y
+    
+    a = -128;
+    b = 128;
+    translateX = a + (b-a).*rand(1,1);
+    translateY = a + (b-1).*rand(1,1);
     
     % Applico le rotazioni
     rotate(volSlice, [1 0 0], rotationX);
@@ -23,19 +28,23 @@ function [img, imgSeg] = reformat(mr, segmentazione)
     zs = get(volSlice, 'ZData');
     delete(volSlice);
 
-    surface = slice(x, y, z, mr, xs, ys, zs + 256);
+    surface = slice(x, y, z, mr, xs + translateX, ys + translateY, zs + 128);
     img = surface.CData;
     img(isnan(img)) = 0;
 
-    segSurf = slice(x, y, z, segmentazione, xs, ys, zs + 256);
+    segSurf = slice(x, y, z, segmentazione, xs + translateX, ys + translateY, zs + 128);
     imgSeg = segSurf.CData;
     imgSeg(isnan(imgSeg)) = 0;
     imgSeg(imgSeg < 1) = 0;
     
+    xs = xs + translateX;
+    ys = ys + translateY;
+    zs = zs + 128;
+    %{
     figure;
-%{
+
     imshow(img, []);
     figure;
     imshow(imgSeg, []);
-%}
+    %}
 end
